068a0f7d5c9931bb14b54f1721cf39ed63d86344
Sourabh Khurana <sourabhkhurana0693@gmail.com>
2020-05-02 19:16:47 +0530

    first bad version

---
+++ First Bad Version.js
@@ -0,0 +1,43 @@
+/**
+ * First Bad Version
+ * Question Link
+ * https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3316/ 
+ */
+
+
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        if (n == 1)
+            return 1;
+        let start = 1;
+        let end = n;
+        let count = 0;
+        while (start <= end) {
+            let mid = Math.floor((start + end) / 2);
+            if (isBadVersion(mid)) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    };
+};
